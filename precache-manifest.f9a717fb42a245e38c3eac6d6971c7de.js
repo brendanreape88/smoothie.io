ad542975bf21a8494d9c94795b7da8757f2e2a50
Brendan Reape <brendanreape88@gmail.com>
2020-07-01 14:40:50 -0700

    Updates

---
+++ precache-manifest.f9a717fb42a245e38c3eac6d6971c7de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e474d4d07bd80e6fc92f1fa720d26e45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bdc940d2375e43e22de",
+    "url": "/static/css/main.4c8b68ba.chunk.css"
+  },
+  {
+    "revision": "1348ee6f790082671123",
+    "url": "/static/js/2.eb95c7cc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.eb95c7cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bdc940d2375e43e22de",
+    "url": "/static/js/main.f56f886b.chunk.js"
+  },
+  {
+    "revision": "0008375b8691a84687a1",
+    "url": "/static/js/runtime-main.af5fedf9.js"
+  }
+]);
