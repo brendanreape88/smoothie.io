33fd8d67cfcde121e1f9443883ad60e4f95a3568
Brendan Reape <brendanreape88@gmail.com>
2020-08-17 11:39:50 -0700

    adding favicon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,54 +1,50 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import SmoothieContext from '../../contexts/SmoothieContext'
-import './Header.css'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import SmoothieContext from "../../contexts/SmoothieContext";
+import "./Header.css";
 
 class Header extends Component {
+  static contextType = SmoothieContext;
 
-    static contextType = SmoothieContext
+  logOut = (event) => {
+    this.context.logOut();
+  };
 
-    logOut = (event) => {
-        this.context.logOut()
-    }
-
-    render() {
-        const user = this.context.user
-        return (
-            <nav className='Header'>
-                <h1>
-                    <Link to='/'>
-                    Smoothie.io
-                    </Link>
-                </h1>
-                <div className='Header__Menu'>
-                    <span>
-                        {user
-                            ? (
-                                <Link to='/' onClick={this.logOut}>
-                                    logout
-                                </Link>
-                            )
-                            : (
-                                <Link to='/login'>
-                                    login
-                                </Link>
-                            )
-                        } 
-                    </span>
-                    <span>
-                        <Link to='/home'>
-                            smoothies
-                        </Link>
-                    </span>
-                    <span>
-                        <Link to='/create'>
-                            create
-                        </Link>
-                    </span>
-                </div>
-            </nav>
-        )
-    }
+  render() {
+    const user = this.context.user;
+    return (
+      <nav className="Header">
+        <div className="Header__Logo">
+          <div className="Icon">
+            <FontAwesomeIcon icon="fa-leaf" />
+          </div>
+          <div className="Title">
+            <h1>
+              <Link to="/">Smoothie.io</Link>
+            </h1>
+          </div>
+        </div>
+        <div className="Header__Menu">
+          <span>
+            {user ? (
+              <Link to="/" onClick={this.logOut}>
+                logout
+              </Link>
+            ) : (
+              <Link to="/login">login</Link>
+            )}
+          </span>
+          <span>
+            <Link to="/home">smoothies</Link>
+          </span>
+          <span>
+            <Link to="/create">create</Link>
+          </span>
+        </div>
+      </nav>
+    );
+  }
 }
 
-export default Header
+export default Header;
