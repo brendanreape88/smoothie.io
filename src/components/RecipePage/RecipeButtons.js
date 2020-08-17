4330f3e908ebd91c64e7029592d0022975a15de2
Brendan Reape <brendanreape88@gmail.com>
2020-08-17 10:38:09 -0700

    removed log

--- src/components/RecipePage/RecipeButtons.js
+++ src/components/RecipePage/RecipeButtons.js
@@ -1,64 +1,56 @@
-import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
-import SmoothieContext from '../../contexts/SmoothieContext'
-import './RecipePage.css'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import SmoothieContext from "../../contexts/SmoothieContext";
+import "./RecipePage.css";
 
 class RecipeButtons extends Component {
+  static contextType = SmoothieContext;
 
-    static contextType = SmoothieContext
-
-    render() {
-        const { recipe={} } = this.props;
-        console.log(recipe)
-        return (
-            <section className="RecipePage__BottomButtons">
-                <div className="RecipePage__BottomButtons__FlexBox">
-                    {('smoothie' in recipe) 
-                        ?(
-                            <>
-                                <button
-                                    onClick={() => this.props.favorite(recipe.smoothie.recipe_id)}
-
-                                >
-                                    favorite
-                                </button>
-                                <button>
-                                    <Link to={{
-                                        pathname: `/create`,
-                                        prepopulate: true,
-                                        customId: `${recipe.smoothie.recipe_id}`
-                                    }}>
-                                        customize
-                                    </Link>
-                                </button>
-                                <button onClick={this.props.click}>
-                                    <Link to={{
-                                        pathname: `/recipe/${recipe.smoothie.recipe_id}#ReviewForm`,
-                                        reviewOn: true
-                                    }}>
-                                        review
-                                    </Link>
-                                </button>
-                            </>
-                        )
-                        :(
-                            <>
-                                <button>
-                                    favorite
-                                </button>
-                                <button>
-                                    customize
-                                </button>
-                                <button>
-                                    review
-                                </button>
-                            </>
-                        )
-                    }
-                </div>
-            </section>
-        )
-    }
+  render() {
+    const { recipe = {} } = this.props;
+    return (
+      <section className="RecipePage__BottomButtons">
+        <div className="RecipePage__BottomButtons__FlexBox">
+          {"smoothie" in recipe ? (
+            <>
+              <button
+                onClick={() => this.props.favorite(recipe.smoothie.recipe_id)}
+              >
+                favorite
+              </button>
+              <button>
+                <Link
+                  to={{
+                    pathname: `/create`,
+                    prepopulate: true,
+                    customId: `${recipe.smoothie.recipe_id}`,
+                  }}
+                >
+                  customize
+                </Link>
+              </button>
+              <button onClick={this.props.click}>
+                <Link
+                  to={{
+                    pathname: `/recipe/${recipe.smoothie.recipe_id}#ReviewForm`,
+                    reviewOn: true,
+                  }}
+                >
+                  review
+                </Link>
+              </button>
+            </>
+          ) : (
+            <>
+              <button>favorite</button>
+              <button>customize</button>
+              <button>review</button>
+            </>
+          )}
+        </div>
+      </section>
+    );
+  }
 }
 
-export default RecipeButtons
+export default RecipeButtons;
